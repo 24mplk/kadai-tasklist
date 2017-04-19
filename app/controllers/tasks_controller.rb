306281bd428e708856edd54e4bcbe5398bf8bc83
mpl.k <24.k.mpl@gmail.com>
2017-04-19 05:10:30 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,59 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task =Task.new(content: 'Get them done!')
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Successfully posted.'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Could not posted.'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Successfully edited.'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Could not edited.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Successfully deleted.'
+    redirect_to tasks_url
+  end
+  
+  private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+  
+  
+end
